ff35e79cd2a9843554002595a919cb26e73f4761
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-15 19:34:59 -0400

    won't render notes on line 20 in index.js + added dirname

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 //Serving static files
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 
 //Installed ejs and created a file inside the views
 app.set('view engine', 'ejs');
