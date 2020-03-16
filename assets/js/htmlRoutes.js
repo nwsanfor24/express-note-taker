31c1ac12af4fffa4de6219f7f9588990d8b70bb6
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-15 22:21:42 -0400

    html routes added

---
+++ assets/js/htmlRoutes.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+// Routes
+// =====================================================================
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
